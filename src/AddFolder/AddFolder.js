b66b0ced32a2f11e0d00d45032495ec4263a9778
adelinel <adeline.latruwe@gmail.com>
2021-02-08 09:53:50 -0800

    added folder validation form

--- src/AddFolder/AddFolder.js
+++ src/AddFolder/AddFolder.js
@@ -3,14 +3,36 @@ import NotefulForm from "../NotefulForm/NotefulForm";
 import ApiContext from "../ApiContext";
 import config from "../config";
 import "./AddFolder.css";
+import ValidationError from "../ValidationError/ValidationError.js";
 
 export default class AddFolder extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+      touched: false,
+    };
+  }
+
   static defaultProps = {
     history: {
       push: () => {},
     },
   };
   static contextType = ApiContext;
+
+  // folder name validation
+  updateFolderName(name) {
+    this.setState({ value: name, touched: true });
+    console.log(name);
+  }
+
+  validateFolderName() {
+    const name = this.state.value;
+    if (name.length === 0) {
+      return "Please enter a folder name";
+    }
+  }
 
   handleSubmit = (e) => {
     e.preventDefault();
@@ -38,18 +60,29 @@ export default class AddFolder extends Component {
   };
 
   render() {
+    const folderNameError = this.validateFolderName();
     return (
       <section className="AddFolder">
         <h2>Create a folder</h2>
-        <NotefulForm onSubmit={this.handleSubmit}>
+        <form onSubmit={this.handleSubmit}>
           <div className="field">
             <label htmlFor="folder-name-input">Name</label>
-            <input type="text" id="folder-name-input" name="folder-name" />
+            <input
+              type="text"
+              id="folder-name-input"
+              name="folder-name"
+              onChange={(e) => this.updateFolderName(e.target.value)}
+            />
+            {this.state.touched && (
+              <ValidationError message={folderNameError} />
+            )}
           </div>
           <div className="buttons">
-            <button type="submit">Add folder</button>
+            <button type="submit" disabled={this.validateFolderName()}>
+              Add folder
+            </button>
           </div>
-        </NotefulForm>
+        </form>
       </section>
     );
   }
